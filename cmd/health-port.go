ea187c20910d3f73e42b819d6a966d9bd4a8fb4e
pschu <peter.schulten@dbschenker.com>
2019-11-03 21:04:25 +0100

    polish

--- cmd/health-port.go
+++ cmd/health-port.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"fmt"
 	server2 "github.com/pschulten/health-port/server"
+	"github.com/spf13/cobra"
 	"log"
 	"net/http"
 	"os"
@@ -10,9 +12,40 @@ import (
 	"time"
 )
 
+var (
+	addr     string
+	interval time.Duration
+	endpoint string
+)
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&addr, "addr", ":1161", "Port to expose")
+	rootCmd.PersistentFlags().DurationVar(&interval, "interval", 2*time.Second, "The amount of time between health checks")
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "health-port health_check_url",
+	Short: "health-port - expose semantic health check",
+	Long:  `health-port - expose semantic health check by running a dummy server on healthiness`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		endpoint = args[0]
+		main()
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	hps := server2.HealthPortServer{
-		Addr: ":1161",
+		Addr: addr,
 	}
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
@@ -24,27 +57,29 @@ func main() {
 
 	running := false
 	for {
-		log.Println("** main begin loop")
-		time.Sleep(time.Second)
+
+		time.Sleep(interval)
 
 		if check() {
 			if !running {
+				log.Printf("healthy endpoint: %s\n", endpoint)
 				go hps.ListenAndServe()
 			}
 			running = true
 		} else {
 			if running {
+				log.Printf("unhealthy endpoint: %s\n", endpoint)
 				hps.Shutdown()
 			}
 			running = false
 		}
-		log.Println("** main end loop")
 	}
 
 }
 
 func check() bool {
-	res, err := http.Get("http://localhost:2015/index.html")
+	//res, err := http.Get("http://localhost:2015/index.html")
+	res, err := http.Get(endpoint)
 	if err != nil {
 		return false
 	}
